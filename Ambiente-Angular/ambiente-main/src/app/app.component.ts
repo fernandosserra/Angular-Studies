ed3df8caf41334db6ab9c10fda919515c841f4df
fernandosserra <fs.serra@outlook.com>
2022-04-24 17:27:52 -0300

    Directives course done

--- Ambiente-Angular/ambiente-main/src/app/app.component.ts
+++ Ambiente-Angular/ambiente-main/src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  count: number = 0;
+
+  myText: string = "";
   
+  pessoas = [
+    {
+      nome: "Melquisedeque",
+      sobrenome: "Taveira"
+    },
+    {
+      nome: "Ananias",
+      sobrenome: "Pacheco"
+    },
+    {
+      nome: "Aloísio",
+      sobrenome: "Romão"
+    },
+    {
+      nome: "Quitéria",
+      sobrenome: "Pires Cavalcante"
+    }
+  ];
+
+  constructor(){};
+
+  ngOnInit(){
+    let interval = setInterval(() => {
+      this.count++;
+      if(this.count === 10){
+        clearInterval(interval);
+      }
+    }, 1000)
+  }
+
+  clicou(name: string): void {
+    console.log(name, 'clicou em mim');
+  }
+
 }
